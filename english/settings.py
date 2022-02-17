2250e6f0ac027f7ba2d2da50bc9113b0d2da4c9a
MykhailoVV <vereshchaka.m@gmail.com>
2022-02-17 16:17:09 +0200

    commit for heroku

--- english/settings.py
+++ english/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')*c^&hxc1k%+$b0_j)w)$w%47po3as@2_9-eo52^ois%)oz=)-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
